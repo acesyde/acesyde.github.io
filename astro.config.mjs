80a62e0f86dbbd89fa1d2741e2c7408de7c692cf
acesyde <acesyde@gmail.com>
2026-01-14 14:08:24 +0100

    chore: code review

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import markdoc from '@astrojs/markdoc';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), markdoc(), mdx(), sitemap()]
+});
